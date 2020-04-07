03974f704e5385d300ece3b6c2b4f5219dbb538a
Salman <50276554+MrHooRa@users.noreply.github.com>
2020-04-07 14:54:14 +0300

    Create Dairy.java

---
+++ Dairy.java
@@ -0,0 +1,72 @@
+/*
+* Salman Alessa
+* UID: 439100011
+*/
+
+// Packages
+import java.util.Scanner;
+
+public class Dairy {
+	// Variables
+	private int numMilk;
+	private int numCheese;
+	private int numButter;
+	private int age;
+	private double priceMilk;
+	private double priceCheese;
+	private double priceButter;
+
+	// Methods
+	static Scanner input = new Scanner(System.in);
+
+	// Setter methods
+	public void prices(double priceMilk, double priceCheese, double priceButter) {
+
+		this.priceMilk = priceMilk;
+		this.priceCheese = priceCheese;
+		this.priceButter = priceButter;
+
+	}
+	
+	public Dairy(int numMilk, int numCheese, int numButter) {
+		this.numMilk = numMilk;
+		this.numCheese = numCheese;
+		this.numButter = numButter;
+	}
+	
+	public void age(int age) {
+		this.age = age;
+	}
+
+	// Calculate dairy price 
+	public double calcDairyPrice(double priceMilk, double priceCheese, double priceButter) {
+		double total;
+		
+		priceMilk = this.numMilk * this.priceMilk;
+		priceCheese = this.numCheese * this.priceCheese;
+		priceButter = this.numButter * this.priceButter;
+		
+		total = priceMilk + priceCheese + priceButter;
+		
+		return total;
+	}
+	
+	public double discountDairy(double total, int age) {
+		// If age >= 65 applied discount
+		if (age >= 65)
+			total = total - (total * 0.20);
+		
+		return total;
+	}
+	
+	//return dairy
+	public String printDairy() {
+		
+		String output = "\n|\t" + this.numMilk + " - Milk for " + priceMilk + "\t\t|"
+				 + "\n|\t" + this.numCheese + " - Cheese for " + priceCheese + "\t\t|"
+				 + "\n|\t" + this.numButter + " - Butter for " + priceButter + "\t\t|";
+		return output;
+		
+	}
+
+}
