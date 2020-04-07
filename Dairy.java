a4389123171ebfeed7c19913984ddb0680372562
Salman <50276554+MrHooRa@users.noreply.github.com>
2020-04-07 21:58:10 +0300

    Update Dairy.java

--- Dairy.java
+++ Dairy.java
@@ -21,19 +21,17 @@ public class Dairy {
 
 	// Setter methods
 	public void prices(double priceMilk, double priceCheese, double priceButter) {
-
 		this.priceMilk = priceMilk;
 		this.priceCheese = priceCheese;
 		this.priceButter = priceButter;
-
 	}
-	
+
 	public Dairy(int numMilk, int numCheese, int numButter) {
 		this.numMilk = numMilk;
 		this.numCheese = numCheese;
 		this.numButter = numButter;
 	}
-	
+
 	public void age(int age) {
 		this.age = age;
 	}
@@ -41,16 +39,15 @@ public class Dairy {
 	// Calculate dairy price 
 	public double calcDairyPrice(double priceMilk, double priceCheese, double priceButter) {
 		double total;
-		
+
 		priceMilk = this.numMilk * this.priceMilk;
 		priceCheese = this.numCheese * this.priceCheese;
 		priceButter = this.numButter * this.priceButter;
-		
 		total = priceMilk + priceCheese + priceButter;
-		
+
 		return total;
 	}
-	
+
 	public double discountDairy(double total, int age) {
 		// If age >= 65 applied discount
 		if (age >= 65)
@@ -58,15 +55,13 @@ public class Dairy {
 		
 		return total;
 	}
-	
+
 	//return dairy
 	public String printDairy() {
-		
 		String output = "\n|\t" + this.numMilk + " - Milk for " + priceMilk + "\t\t|"
 				 + "\n|\t" + this.numCheese + " - Cheese for " + priceCheese + "\t\t|"
 				 + "\n|\t" + this.numButter + " - Butter for " + priceButter + "\t\t|";
-		return output;
-		
+		return output;	
 	}
 
 }
