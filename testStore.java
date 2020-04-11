cad32dfd3e36f96ecb1b38d33e74c272975d473b
Salman <50276554+MrHooRa@users.noreply.github.com>
2020-04-11 11:28:55 +0300

    Update testStore.java

--- testStore.java
+++ testStore.java
@@ -13,15 +13,15 @@ public class testStore {
 	public static void options(int age, double priceMilk, 
 			double priceCheese, double priceButter, double priceMuffins,
 			double priceCroissants, double priceBiscuits) {
-		
+
 		System.out.print("\n********************************************\n" +
 				 		 "** Enter [1]-Dairy   [2]-Bakery  [0]-Exit **\n" + 
 						 "********************************************\n > ");
 		int option = input.nextInt();
-		
+
 		// If user select Dairy
 		if (option == 1) {
-			
+
 			// Ask user..
 			System.out.print("Enter number of milk you want to buy: ");
 			int numMilk = input.nextInt();		
@@ -31,20 +31,16 @@ public class testStore {
 			int numButter = input.nextInt();	
 
 			// New object (objDairy)
-
-			// Setter
 			Dairy objDairy = new Dairy(numMilk, numCheese, numButter);
 			objDairy.prices(priceMilk, priceCheese, priceButter);
-			objDairy.age(age);
 
-			// This is complex , this line calc total and sum it with Dairy 
+			// This is complex , this line calculate total and sum it with Dairy 
 			total = total + objDairy.discountDairy(objDairy.calcDairyPrice(priceMilk, priceCheese, priceButter), age);
 
 			Dairy = objDairy.printDairy();
 			options(age, 
 					priceMilk, priceCheese, priceButter, 
 					priceMuffins, priceCroissants, priceBiscuits);
-
 
 		// If user select Bakery
 		}else if (option == 2) {
@@ -55,15 +51,13 @@ public class testStore {
 			System.out.print("Enter number of biscuits you want to buy: ");
 			int numBiscuits = input.nextInt();
 
-
-			// New object (objDairy) 
+			// New object (objBakery) 
 			Bakery objBakery = new Bakery(numMuffins, numCroissants, numBiscuits);
-			objBakery.prices(priceMilk, priceCheese, priceButter);
-			objBakery.age(age);
+			objBakery.prices(priceMuffins, priceCroissants, priceBiscuits);
 
-			// This is complex , this line calc total and sum it with Bakery 
+			// This is complex , this line calculate total and sum it with Bakery 
 			total = total + objBakery.discountBakery(objBakery.calcBakeryPrice(priceMuffins, priceCroissants, priceBiscuits), age);
-			
+
 			Bakery = objBakery.printBakery();
 			options(age, 
 					priceMilk, priceCheese, priceButter, 
@@ -94,7 +88,7 @@ public class testStore {
 						+ "\n\nYour total after discount: "
 						+ total);
 			}
-		
+
 		// If user select wrong option
 		}else {
 			System.out.print("Please select the correct option from menu");
@@ -106,57 +100,58 @@ public class testStore {
 
 	// This method return integer
 	// This method check if user enter incorrect values
-	// Set value -> -1
-	public static int CheckI(String text, int min, int max, int value) {
+	// Set default value -> -1
+	public static int CheckInt(String text, int min, int max, int value) {
 		if (value < min || value > max) {
 			System.out.print(text);
 			value = input.nextInt();
-			CheckI(text, min, max, value);
+			CheckInt(text, min, max, value);
 		}else return value;
 		return value;
 	}
-	
+
 	// This method return double
 	// This method check if user enter incorrect values
-	// Set value -> -1
-	public static double CheckD(String text, int min, int max, double value) {
+	// Set default value -> -1
+	public static double CheckDouble(String text, int min, int max, double value) {
 		if (value < min || value > max) {
 			System.out.print(text);
 			value = input.nextDouble();
-			CheckD(text, min, max, value);
+			CheckDouble(text, min, max, value);
 		}else return value;
 		return value;
 	}
-	
+
 	public static void main(String[] args) {
-		// Local vars
-		int day, age;
+		// Local Variables
+		int age;
 		double priceMilk, priceCheese, priceButter, priceMuffins, priceCroissants, priceBiscuits;		
 
-		// Welcome msg and ask user to enter day, age, price of milk, price of cheese, price of butter, price of muffins, price of croissants and price of biscuits.
+		// Welcome message then ask user to enter day, age, price of milk, cheese, butter, muffins, croissants and biscuits.
 		// After that, ask user to choose one option.
 		System.out.print("****************************************************************\n" + 
 						 "**                Welcome to the CSC 111 store                **\n" + 
 						 "****************************************************************\n");
 
-		// Method Check(String text, int min, int max, int value)
+		// Method Check***(String text, int min, int max, int value)
 		// This method check if user enter incorrect values
 
-		day = CheckI("Please enter the day: 1-Sunday, 2-Monday, so on till 7-Saturday: ", 0, 7, -1);
+		// I think we do not need day variable, so I put checkInt without variable!
+		CheckInt("Please enter the day: 1-Sunday, 2-Monday, so on till 7-Saturday: ", 1, 7, -1);
 
-		age = CheckI("Please enter coustmer age: ", 0, 150, -1);
+		age = CheckInt("Please enter coustmer age: ", 1, 150, -1);
 
-		priceMilk = CheckD("Enter today's price of milk-> ", 0, 100, -1);
+		priceMilk = CheckDouble("Enter today's price of milk-> ", 0, 100, -1);
 
-		priceCheese = CheckD("Enter today's price of cheese-> ", 0, 999, -1);
+		priceCheese = CheckDouble("Enter today's price of cheese-> ", 0, 999, -1);
 
-		priceButter = CheckD("Enter today's price of butter-> ", 0, 999, -1);
+		priceButter = CheckDouble("Enter today's price of butter-> ", 0, 999, -1);
 
-		priceMuffins = CheckD("Enter today's price of muffin-> ", 0, 999, -1);
+		priceMuffins = CheckDouble("Enter today's price of muffin-> ", 0, 999, -1);
 
-		priceCroissants = CheckD("Enter today's price of crosissants-> ", 0, 999, -1);
+		priceCroissants = CheckDouble("Enter today's price of crosissants-> ", 0, 999, -1);
 
-		priceBiscuits = CheckD("Enter today's price of biscuit-> ", 0, 999, -1);
+		priceBiscuits = CheckDouble("Enter today's price of biscuit-> ", 0, 999, -1);
 
 		options(age, 
 				priceMilk, priceCheese, priceButter, 
